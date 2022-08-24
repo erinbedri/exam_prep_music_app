6e787f12a79850afd74feb50eb9cbf9cf4522cb3
erinbedri <erinbedri@gmail.com>
2022-08-24 13:29:12 +0200

    added 2 questions from admin panel

--- practice_django/polls/views.py
+++ practice_django/polls/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Hello')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_questions])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s.' % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
