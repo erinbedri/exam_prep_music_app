2fc7649ae97db0e40843f177980f71e32f1e940e
erinbedri <erinbedri@gmail.com>
2022-09-13 11:33:36 +0200

    added room description

--- MeetMe/base/views.py
+++ MeetMe/base/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 rooms = [
-    {'id': 1, 'name': 'Python'},
-    {'id': 2, 'name': 'JS'},
-    {'id': 3, 'name': 'Java'},
+    {'id': 1, 'name': 'Python', 'description': 'This is the Python Room'},
+    {'id': 2, 'name': 'JS', 'description': 'This is the JS Room'},
+    {'id': 3, 'name': 'Java', 'description': 'This is the Java Room'},
 ]
 
 
