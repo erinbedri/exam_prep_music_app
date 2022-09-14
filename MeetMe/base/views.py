92e1a9861eb52394501e711f2a2b7c4c6c706349
erinbedri <erinbedri@gmail.com>
2022-09-14 21:43:14 +0200

    created topic and message models

--- MeetMe/base/views.py
+++ MeetMe/base/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'Python', 'description': 'This is the Python Room'},
-    {'id': 2, 'name': 'JS', 'description': 'This is the JS Room'},
-    {'id': 3, 'name': 'Java', 'description': 'This is the Java Room'},
-]
+from base.models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
 
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
-
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
