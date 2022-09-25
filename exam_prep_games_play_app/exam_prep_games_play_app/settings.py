3f8c24b3e3688188da8dae56cdf3604657fed3ab
erinbedri <erinbedri@gmail.com>
2022-09-25 15:04:02 +0200

    initial setup

--- exam_prep_games_play_app/exam_prep_games_play_app/settings.py
+++ exam_prep_games_play_app/exam_prep_games_play_app/settings.py
@@ -60,14 +60,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'exam_prep_games_play_app.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '930502erin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
