5ce5d960029921c9cae9675b79d25ccbc57cfa80
erinbedri <erinbedri@gmail.com>
2022-09-25 15:59:13 +0200

    models setup

--- exam_prep_games_play_app/exam_prep_games_play_app/web/admin.py
+++ exam_prep_games_play_app/exam_prep_games_play_app/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from exam_prep_games_play_app.web.models import Profile, Game
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age')
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'rating')
+
--- exam_prep_games_play_app/exam_prep_games_play_app/web/models.py
+++ exam_prep_games_play_app/exam_prep_games_play_app/web/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -38,7 +38,59 @@ class Profile(models.Model):
         blank=True,
     )
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
+class Game(models.Model):
+    TITLE_MAX_LEN = 30
 
+    CATEGORY_CHOICES = (
+        ('Action', 'Action'),
+        ('Adventure', 'Adventure'),
+        ('Puzzle', 'Puzzle'),
+        ('Strategy', 'Strategy'),
+        ('Sports', 'Sports'),
+        ('Board/Card Game', 'Board/Card Game'),
+        ('Other', 'Other'),
+    )
 
+    CATEGORY_MAX_LEN = max([len(c[1]) for c in CATEGORY_CHOICES])
+
+    RATING_MIN = 0.1
+    RATING_MAX = 5
+
+    MAX_LEVEL_MIN_VALUE = 1
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LEN,
+        unique=True,
+    )
+
+    category = models.CharField(
+        max_length=CATEGORY_MAX_LEN,
+        choices=CATEGORY_CHOICES,
+    )
+
+    rating = models.FloatField(
+        validators=[
+            MinValueValidator(RATING_MIN),
+            MaxValueValidator(RATING_MAX),
+        ]
+    )
+
+    max_level = models.IntegerField(
+        validators=[
+            MinValueValidator(MAX_LEVEL_MIN_VALUE)
+        ]
+    )
+
+    image = models.URLField()
+
+    summary = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.title}'
