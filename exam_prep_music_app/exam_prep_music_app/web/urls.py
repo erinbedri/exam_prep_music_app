717aa5c1ee474baa4504c5fc6674a306aee4be3b
erinbedri <erinbedri@gmail.com>
2022-09-27 09:54:55 +0200

    initial setup

--- exam_prep_music_app/exam_prep_music_app/web/urls.py
+++ exam_prep_music_app/exam_prep_music_app/web/urls.py
@@ -4,4 +4,12 @@ from exam_prep_music_app.web import views
 
 urlpatterns = [
     path('', views.show_homepage, name='show homepage'),
+
+    path('album/add/', views.add_album, name='add album page'),
+    path('album/details/<int:id>/', views.show_album, name='show album details page'),
+    path('album/edit/<int:id>/', views.edit_album, name='edit album page'),
+    path('album/delete/<int:id>/', views.delete_album, name='delete album page'),
+
+    path('profile/details/', views.show_profile, name='show profile details page'),
+    path('profile/delete/', views.delete_profile, name='delete profile page'),
 ]
--- exam_prep_music_app/exam_prep_music_app/web/views.py
+++ exam_prep_music_app/exam_prep_music_app/web/views.py
@@ -2,4 +2,57 @@ from django.shortcuts import render
 
 
 def show_homepage(request):
-    pass
+    context = {
+
+    }
+
+    return render(request, 'home-page.html', context)
+
+
+def add_album(request):
+    context = {
+
+    }
+
+    return render(request, 'crea', context)
+
+
+def show_album(request):
+    context = {
+
+    }
+
+    return render(request, '', context)
+
+
+def edit_album(request):
+    context = {
+
+    }
+
+    return render(request, '', context)
+
+
+def delete_album(request):
+    context = {
+
+    }
+
+    return render(request, '', context)
+
+
+def show_profile(request):
+    context = {
+
+    }
+
+    return render(request, '', context)
+
+
+def delete_profile(request):
+    context = {
+
+    }
+
+    return render(request, '', context)
+
